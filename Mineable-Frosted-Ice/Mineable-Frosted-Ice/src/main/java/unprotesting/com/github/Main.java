60f7b0b8c3d1535e5c2626407c1be54a43749224
Unprotesting <unprotesting.email@gmail.com>
2020-05-18 12:06:29 +0100

    Added bStats
    + Added bStats integration: https://bstats.org/plugin/bukkit/Mineable-Frosted-Ice/7570

--- Mineable-Frosted-Ice/Mineable-Frosted-Ice/src/main/java/unprotesting/com/github/Main.java
+++ Mineable-Frosted-Ice/Mineable-Frosted-Ice/src/main/java/unprotesting/com/github/Main.java
@@ -2,6 +2,8 @@ package unprotesting.com.github;
 
 import java.io.File;
 import java.util.logging.Logger;
+
+import org.bstats.bukkit.Metrics;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -29,7 +31,9 @@ public class Main extends JavaPlugin implements Listener{
         config.addDefault("Silk-Touch Required", true);
         config.options().copyDefaults(true);
         saveConfig();
+        int pluginId = 7570;
         plugin = this;
+        Metrics metrics = new Metrics(this, pluginId);
         boolean enabled = getConfig().getBoolean("Enabled");
         if (enabled == false) {
             log.severe(String.format("Disabled plugin as defined in config!", getDescription().getName()));
